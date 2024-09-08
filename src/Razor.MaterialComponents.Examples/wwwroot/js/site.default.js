d66749790d126d11aa44185a524339eeff32284b
Christopher Sampson <sammosampson@gmail.com>
2024-09-08 16:04:40 +0100

    migration

---
+++ src/Razor.MaterialComponents.Examples/wwwroot/js/site.default.js
@@ -0,0 +1,106 @@
+﻿/* 
+    This file contains the default site-wide scripts  
+*/
+const activeCharts = [];
+
+function hookup_chart(element) {
+    var config = JSON.parse(document.getElementById(`${element.id}-chart-script`).textContent);
+    activeCharts.push(new Chart(element, config));
+}
+
+function hookup_charts() {
+    const charts = document.querySelectorAll('.chartjs-chart');
+    for (const chart of charts) {
+        hookup_chart(chart);
+    }
+}
+
+function unhookup_charts() {
+    for (const chart of activeCharts) {
+        chart.destroy();
+    }
+    activeCharts.clear
+}
+
+function hookup_mdc_appbar_drawer() {
+    const drawerElement = document.querySelector('.mdc-drawer');
+    if (drawerElement === null) {
+        return;
+    }
+    const drawer = mdc.drawer.MDCDrawer.attachTo(drawerElement);
+    const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(document.querySelector('.mdc-top-app-bar'));
+    topAppBar.listen('MDCTopAppBar:nav', () => {
+        drawer.open = !drawer.open;
+    });
+}
+
+function hookup_mdc_banners() {
+    const banners = document.querySelectorAll('.mdc-banner');
+    for (const bannerElment of banners) {
+        const banner = mdc.banner.MDCBanner.attachTo(bannerElment);
+        banner.open();
+    }
+}
+
+function hookup_mdc_buttons() {
+    const buttons = document.querySelectorAll('.mdc-button');
+    for (const button of buttons) {
+        mdc.ripple.MDCRipple.attachTo(button);
+    }
+}
+
+function hookup_mdc_tables() {
+    const buttons = document.querySelectorAll('.mdc-data-table');
+    for (const button of buttons) {
+        mdc.dataTable.MDCDataTable.attachTo(button);
+    }
+}
+
+function hookup_mdc_dialogs() {
+    const elements = document.querySelectorAll('.mdc-dialog');
+    for (const element of elements) {
+        mdc.dialog.MDCDialog.attachTo(element);
+    }
+}
+
+function hookup_mdc_selects() {
+    const selects = document.querySelectorAll('.mdc-select');
+    for (const select of selects) {
+        mdc.select.MDCSelect.attachTo(select);
+    }
+}
+
+function hookup_mdc_text_fields() {
+    const textFields = document.querySelectorAll('.mdc-text-field');
+    for (const textField of textFields) {
+        const banner = mdc.textField.MDCTextField.attachTo(textField);
+    }
+}
+
+function hookup() {
+    hookup_charts();
+    hookup_mdc_appbar_drawer();
+    hookup_mdc_banners();
+    hookup_mdc_buttons();
+    hookup_mdc_tables();
+    hookup_mdc_dialogs();
+    hookup_mdc_text_fields();
+    hookup_mdc_selects();
+}
+
+function unhookup() {
+    unhookup_charts();
+}
+
+function hookup_htmx_events() {
+    document.addEventListener('htmx:afterSettle', function (evt) {
+        hookup();
+    });
+
+    document.addEventListener('htmx:beforeRequest', function (evt) {
+        unhookup();
+    });
+}
+
+hookup_htmx_events();
+hookup();
